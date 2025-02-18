31d271d46378486a4dfdef85679c40f9781f4aa0
LongDude <teplov.vladimir.2004@mail.ru>
2025-02-18 10:07:40 +0300

    update: ★styles★

--- Lab-2/app/index.php
+++ Lab-2/app/index.php
@@ -3,28 +3,28 @@
 <head>
   <meta charset="UTF-8"> 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Такси-сервис "Туда-Сюда"</title>
 <body>
-  <h1>"Туда-Сюда" и уже приехали!</h1>
-  
-  <?php if (!empty($message)): ?>
-    <p><?php echo $message; ?></p>
-  <?php endif; ?>
+  <main>
+    <h1>"Туда-Сюда" и уже приехали!</h1>
 
-  <form action="form.php" method="POST">
-    <label for="name">Имя:</label>
-    <input type='text' name="name" id="name">
-    <br>
-    <label for="phone">Контакстный номер:</label>
-    <input type="text" name="phone" id="phone" pattern="+[0-9]{1,11}">
-    <br>
-    <label for="car_registration">Номера машины:</label>
-    <input type="text" name="car_registration" pattern="\S{1,6}" id="tarifs"> 
-    <br>
-    <label for="tarifs"> Тариф:</label>
-    <input type="text" name="tarifs" pattern="[0-9]{1,4}" id="tarifs"> 
-    <br>
-    <input type="submit" value="Отправить">
-  </form>  
+    
+    <?php if (!empty($message)): ?>
+      <p><?php echo $message; ?></p>
+      <?php endif; ?>
+      
+      <form action="form.php" method="POST">
+        <label for="name">Имя:</label>
+        <input type='text' name="name" id="name">
+        <label for="phone">Контакстный номер:</label>
+        <input type="text" name="phone" id="phone" pattern="+[0-9]{1,11}">
+        <label for="car_registration">Номера машины:</label>
+        <input type="text" name="car_registration" pattern="\S{1,6}" id="tarifs"> 
+        <label for="tarifs"> Тариф:</label>
+        <input type="text" name="tarifs" pattern="[0-9]{1,4}" id="tarifs"> 
+        <input type="submit" value="Отправить" id="submit">
+      </form>  
+    </main>
 <body>
 </html>
